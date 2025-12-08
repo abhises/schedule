6bdb2a40b226f340c08f43fdf7efa71842ee1699
abhises <abhisespoudyal@gmail.com>
2025-12-08 17:39:34 +0545

    removing prisma for now

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,7 @@
 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
-import { syncUser } from "@/lib/syncUser";
+// import { syncUser } from "@/lib/syncUser";
 
 export default async  function Layout({ children }: { children: React.ReactNode }) {
 
--- lib/syncUser.ts
+++ lib/syncUser.ts
@@ -1,42 +1,42 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
-import { prisma } from "./prisma";
+// import { auth, currentUser } from "@clerk/nextjs/server";
+// import { prisma } from "./prisma";
 
-export async function syncUser() {
-  try {
-    const session = await auth(); // session is of type SessionAuthWithRedirect
-  const userId = session.userId; // now TypeScript knows it exists
+// export async function syncUser() {
+//   try {
+//     const session = await auth(); // session is of type SessionAuthWithRedirect
+//   const userId = session.userId; // now TypeScript knows it exists
 
-  if (!userId) return null;
+//   if (!userId) return null;
 
-    // Fetch Clerk user details
-    const clerkUser = await currentUser();
-    if (!clerkUser) return null;
+//     // Fetch Clerk user details
+//     const clerkUser = await currentUser();
+//     if (!clerkUser) return null;
 
-    const email = clerkUser.emailAddresses[0]?.emailAddress || "";
+//     const email = clerkUser.emailAddresses[0]?.emailAddress || "";
 
-    // Sync into DB
-    const user = await prisma.user.upsert({
-      where: { clerkId: clerkUser.id },
-      update: {
-        name: clerkUser.username ?? `${clerkUser.firstName} ${clerkUser.lastName}`,
-        email,
-        firstName: clerkUser.firstName,
-        lastName: clerkUser.lastName,
-        imageUrl: clerkUser.imageUrl,
-      },
-      create: {
-        clerkId: clerkUser.id,
-        name: clerkUser.username ?? `${clerkUser.firstName} ${clerkUser.lastName}`,
-        email,
-        firstName: clerkUser.firstName,
-        lastName: clerkUser.lastName,
-        imageUrl: clerkUser.imageUrl,
-      },
-    });
+//     // Sync into DB
+//     const user = await prisma.user.upsert({
+//       where: { clerkId: clerkUser.id },
+//       update: {
+//         name: clerkUser.username ?? `${clerkUser.firstName} ${clerkUser.lastName}`,
+//         email,
+//         firstName: clerkUser.firstName,
+//         lastName: clerkUser.lastName,
+//         imageUrl: clerkUser.imageUrl,
+//       },
+//       create: {
+//         clerkId: clerkUser.id,
+//         name: clerkUser.username ?? `${clerkUser.firstName} ${clerkUser.lastName}`,
+//         email,
+//         firstName: clerkUser.firstName,
+//         lastName: clerkUser.lastName,
+//         imageUrl: clerkUser.imageUrl,
+//       },
+//     });
 
-    return user;
-  } catch (error) {
-    console.error("Error syncing user:", error);
-    return null;
-  }
-}
+//     return user;
+//   } catch (error) {
+//     console.error("Error syncing user:", error);
+//     return null;
+//   }
+// }
