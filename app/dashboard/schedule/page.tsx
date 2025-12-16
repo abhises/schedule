04ba1c2a010276bba8e60692a1e4eac9e5fed554
abhises <abhisespoudyal@gmail.com>
2025-12-16 17:53:34 +0545

    trying schedule

--- app/dashboard/schedule/page.tsx
+++ app/dashboard/schedule/page.tsx
@@ -1,43 +1,9 @@
-import React from "react";
-import CustomButton from "@/components/ui/custom-button";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import Link from "next/link";
+import React from 'react'
+
 const page = () => {
   return (
-    <div>
-      <div className="flex items-center justify-end w-full">
-        <Link href="/dashboard/schedule/create">
-          {" "}
-          <CustomButton variant="primary">Create Schedule</CustomButton>
-        </Link>
-      </div>
-      <Table>
-        <TableCaption>All team members</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Id</TableHead>
-            <TableHead>ClerkId</TableHead>
-            <TableHead>Email</TableHead>
-            <TableHead>First Name</TableHead>
-            <TableHead>Last Name</TableHead>
-            <TableHead>Image</TableHead>
-            <TableHead>Role</TableHead>
-            <TableHead>CreatedAt</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody></TableBody>
-      </Table>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default page;
+export default page
